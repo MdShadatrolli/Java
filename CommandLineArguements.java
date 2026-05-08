630f1945ca822e08f7d97aa265fd7f833e6e2792
Mohammadshadathusen <shadatrolli05@gmail.com>
2026-05-08 18:11:24 +0530

    Learnt conditionals

--- CommandLineArguements.java
+++ CommandLineArguements.java
@@ -1,7 +1,23 @@
+import java.util.*;
 public class CommandLineArguements {
     public static void main(String [] args){
-        System.out.println(args[0]);
-        
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ") ;
+
+        a = sc.nextInt();
+        System.out.print("Enter b: ");
+        b= sc.nextInt();
+        if(a==b){
+            System.out.print("a and b are equal");
+        }
+        else if(a>b){
+            System.out.print("a is greater than b");
+        }
+        else{
+            System.out.print("a is smaller than b");
+        }
+
     }
     
 }
